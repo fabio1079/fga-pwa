d56bbe3e25e843ec71743a41974959fdcafe4cee
Fabio Teixeira <fabio1079@gmail.com>
2017-08-11 08:49:29 -0300

    settup tests with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const wrapper = shallow(<App />);
+
+  expect(
+    wrapper.contains(<h2>Welcome to React</h2>)
+  ).toBe(true);
 });
